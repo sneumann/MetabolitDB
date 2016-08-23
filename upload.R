a3303375aac7500558e095ef7a634277fe85fe7e
pneefe13 <patrik.neefe@gmx.de>
2016-08-23 12:26:06 +0200

    added preprocessing
    - added preproc function which does the same as all the single lines
    before did; creates data that is suitable for the massage-function ->
    final call upload(massage(preproc("file/path")))

--- upload.R
+++ upload.R
@@ -20,10 +20,11 @@ systems_in_db <- function(){
   return(data_back)
 }
 #test reading of a csv and a tsv
+# StringsAsFactors = F
 csv <- read.table("/data/Metabolite negativ H2O MAF.csv", sep=",") 
 tsv <- read.table("/data/Metabolite negativ H2O MAF.tsv", sep="\t")
-csvH <- read.table("/data/Metabolite negativ H2O MAF.csv", sep=",", header=T) 
-tsvH <- read.table("/data/Metabolite positiv H2O MAF.tsv", sep="\t", header=T)
+csvH <- read.table("/data/Metabolite negativ H2O MAF.csv", sep=",", header=T,  stringsAsFactors = F) 
+tsvH <- read.table("/data/Metabolite positiv H2O MAF.tsv", sep="\t", header=T, stringsAsFactors = F)
 data <- read.table("/data/m_MTBLS160_Exudate_metabolite_profiling_mass_spectrometry_targeted_v2_maf.tsv", sep="\t", header=T)
 #set vector of Predret-colnames, changes are PubChem, Compound and rt
 
@@ -39,6 +40,20 @@ subdata<-tsvH[tsvH$database=='PubChem', ]
 colnames(subdata) <- sub('database_identifier','PubChem',colnames(subdata))
 colnames(subdata) <- sub('metabolite_identification','Compound',colnames(subdata))
 colnames(subdata) <- sub('retention_time','rt',colnames(subdata))
+
+#makes all preprocessing prior to massage(data)
+#takes: *.tsv filepath (in this environment: /data/filename.tsv) of
+#       a MAF-formatted TSV
+#returns: preprocessed dataframe with PredRet-compatible colnames and only
+#         those entries with a PubChem identifier
+preproc <- function(filein) {
+  data <- read.table(filein, sep="\t", header=T, stringsAsFactors = F)
+  subdata<-data[data$database=='PubChem',]
+  colnames(subdata) <- sub('database_identifier','PubChem',colnames(subdata))
+  colnames(subdata) <- sub('metabolite_identification','Compound',colnames(subdata))
+  colnames(subdata) <- sub('retention_time','rt',colnames(subdata))
+  return(subdata)
+}
 
 # functions for producing a tsv and a csv file
 to_tsv <- function(filein, fileout) {
@@ -60,6 +75,23 @@ convert_format <- function(filein, fileout, outtype) {
   else {print("Argument outtype needs to be  {\"tsv\"|\"csv\"}")}
 }
 
+uploaddata <- massage(subdata)
+#sortiere spalten so wie input????
+uploaddata <- uploaddata[c(12,13,10,5,2,11,4,3,14,1,8,9,6,7)]
+
+alldata <- PredRetR:::get_user_data()
+str(alldata)
+
+testcolnames <- c('ci_lower', 'ci_upper', 'generation', 'inchi', 'name', 'predicted_rt', 'pubchem', 'recorded_rt', 'suspect', 'sys_id', 'userID', 'username', 'time') 
+#, 'system')
+testcontent <- c(as.numeric(NA), as.numeric(NA), 0, as.character(NA), 'Testsubstanz', as.numeric(NA), 999999, 13, FALSE, sys_id, 1234, 'Test', time)
+#, 'IPB_Halle')
+testdf <- data.frame(testcontent)
+testdf <- t(testdf)
+colnames(testdf) <- testcolnames
+
+View(testdf)
+
 #upload data to predret function
 PredRet_upload_CSV <- function(data) {
   # Convert data.frame to bson
@@ -67,9 +99,8 @@ PredRet_upload_CSV <- function(data) {
   
   
   # add to table
-  mongo <- PredRet_connect()
-  wrote = mongo.insert.batch(mongo, PredRet.env$namespaces$ns_rtdata,
-                             bson_data)
+  mongo <- PredRet_connect() 
+  wrote = mongo.insert.batch(mongo, PredRet.env$namespaces$ns_rtdata, bson_data)
   del <- mongo.disconnect(mongo)
   del <- mongo.destroy(mongo)
 }
@@ -81,35 +112,13 @@ get_systems()
 #mongo connect to predret db
 mongo <- PredRet_connect()
 
-# IGNORE BELOW, intended for checking uploaded data, didn't work. To see
-# uploaded data, refer to the shiny app @ http://192.168.99.100:8788/ in
-# the respective system.
-
-#fields = mongo.bson.buffer.create()
-#mongo.bson.buffer.append(fields, "_id", 1L)
-#mongo.bson.buffer.append(fields, "sys_id", 1L)
-#mongo.bson.buffer.append(fields, "system_name", 1L)
-#mongo.bson.buffer.append(fields, "system_desc", 1L)
-#mongo.bson.buffer.append(fields, "userID", 1L)
-#mongo.bson.buffer.append(fields, "username", 1L)
-#mongo.bson.buffer.append(fields, "system_eluent", 1L)
-#mongo.bson.buffer.append(fields, "system_eluent_pH", 1L)
-#mongo.bson.buffer.append(fields, "system_eluent_additive", 1L)
-#mongo.bson.buffer.append(fields, "system_column", 1L)
-#mongo.bson.buffer.append(fields, "system_column_type", 1L)
-#mongo.bson.buffer.append(fields, "system_ref", 1L)
-#fields = mongo.bson.from.buffer(fields)
-#data_back = mongo.find.all(mongo, ns = PredRet.env$namespaces$ns_rtdata, fields = fields, mongo.oid2character = TRUE)
-#del <- mongo.disconnect(mongo)
-#del <- mongo.destroy(mongo)
-#return(data_back)
-
-#mongo.find.one(mongo, ns=PredRet.env$namespaces$n_rtdata) #klappt scheinbar?
-
 # funktioniert teilweise?
 testcursor <- mongo.find.all(mongo, ns=PredRet.env$namespaces$ns_rtdata)
 #rtdata_df <- mongo.cursor.to.data.frame(testcursor)
-
 ##########
 ###KLAPPT zum ansehen der daten
 #d <- do.call(rbind, testcursor)
+mongo <-PredRet_connect()
+res <- mongo.find.all(mongo=mongo,ns=PredRet.env$namespaces$ns_rtdata)
+head(res)
+tail(res)
