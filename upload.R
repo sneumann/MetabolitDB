432f700f364669a2b895d624cf47cef0829a06cc
pneefe13 <patrik.neefe@gmx.de>
2016-09-07 12:08:56 +0200

    Cleaned
    Cleaned the file up visually, added function descriptions and moved the
    old testing and upload lines down in their new own section. The
    necessary functions and lines are now directly on top of the file

--- upload.R
+++ upload.R
@@ -1,59 +1,61 @@
-setwd("C:/Users/Patrik/Desktop/data/")
-setwd("/data/")
+#setwd("C:/Users/Patrik/Desktop/data/")
+#setwd("/data/")
 
-
-#Daten ansehen: library + 3 PredRet.env-Parameter setzen
-#-> connect -> unten cursor + dann df (kein echtes df leider...)
-#ueberlebt neustart von compose up
-
+# Necessary preparation of libraries etc
 library(PredRetR)
 library(chemhelper)
+install_github("dgrapov/CTSgetR")
+library(CTSgetR)
 PredRet.env$mongo$host <- "predret-mongo"
 PredRet.env$mongo$username <- ""
 PredRet.env$mongo$password <- ""
 
+# Get systems
 systems_in_db <- function(){
-  # Make sure it updates on submission
-  #input$submit_system
   
   data_back <- get_systems()
   return(data_back)
+
 }
-#test reading of a csv and a tsv
-# StringsAsFactors = F
-csv <- read.table("/data/Metabolite negativ H2O MAF.csv", sep=",") 
-tsv <- read.table("/data/Metabolite negativ H2O MAF.tsv", sep="\t")
-csvH <- read.table("/data/Metabolite negativ H2O MAF.csv", sep=",", header=T,  stringsAsFactors = F) 
-tsvH <- read.table("/data/Metabolite positiv H2O MAF.tsv", sep="\t", header=T, stringsAsFactors = F)
-data <- read.table("/data/m_MTBLS160_Exudate_metabolite_profiling_mass_spectrometry_targeted_v2_maf.tsv", sep="\t", header=T)
-#set vector of Predret-colnames, changes are PubChem, Compound and rt
 
-subdata<-tsvH[tsvH$database=='PubChem', ]
-
-#translating ChEBI to PubChem CID:
-##library(devtools)
-##install_github(repo="CTSgetR", username="dgrapov", ref="simple")
-#library(CTSgetR)
-#values <- CTSgetR(id=subdata$database_identifier,from="ChEBI",to="PubChem CID")
-#subdata$database_identifier<-values$value
-
-colnames(subdata) <- sub('database_identifier','PubChem',colnames(subdata))
-colnames(subdata) <- sub('metabolite_identification','Compound',colnames(subdata))
-colnames(subdata) <- sub('retention_time','rt',colnames(subdata))
-
-#makes all preprocessing prior to massage(data)
-#takes: *.tsv filepath (in this environment: /data/filename.tsv) of
+# Makes all preprocessing prior to massage(data)
+# Takes: *.tsv filepath (in this environment: /data/filename.tsv) of
 #       a MAF-formatted TSV
-#returns: preprocessed dataframe with PredRet-compatible colnames and only
+# Returns: preprocessed dataframe with PredRet-compatible colnames and only
 #         those entries with a PubChem identifier
 preproc <- function(filein) {
+
   data <- read.table(filein, sep="\t", header=T, stringsAsFactors = F)
+  ix <- sapply(data, is.character)
+  data[ix] <- lapply(data[ix], iconv, from="latin1")
   subdata<-data[data$database=='PubChem',]
   colnames(subdata) <- sub('database_identifier','PubChem',colnames(subdata))
   colnames(subdata) <- sub('metabolite_identification','Compound',colnames(subdata))
   colnames(subdata) <- sub('retention_time','rt',colnames(subdata))
   return(subdata)
+  
 }
+
+# Uploads data to the predret database
+# Takes:    the correct dataframe, which is a massaged (massage()), preprocessed
+#           (preproc()) input in the form of a MAF File
+# Returns:  Nothing; it just uploads the data 
+PredRet_upload <- function(data) {
+  
+  # Convert data.frame to bson
+  bson_data = mongo.bson.from.df(data)
+  
+  # Add to table
+  mongo <- PredRet_connect() 
+  wrote = mongo.insert.batch(mongo, PredRet.env$namespaces$ns_rtdata, bson_data)
+  del <- mongo.disconnect(mongo)
+  del <- mongo.destroy(mongo)
+  
+}
+
+# View all data with these lines
+alldata <- PredRetR:::get_user_data()
+str(alldata)
 
 # functions for producing a tsv and a csv file
 to_tsv <- function(filein, fileout) {
@@ -75,13 +77,34 @@ convert_format <- function(filein, fileout, outtype) {
   else {print("Argument outtype needs to be  {\"tsv\"|\"csv\"}")}
 }
 
-uploaddata <- massage(subdata)
-#sortiere spalten so wie input????
-uploaddata <- uploaddata[c(12,13,10,5,2,11,4,3,14,1,8,9,6,7)]
+#######################################################################
+#     ATTENTION                                                       #
+#     From here on, there are only old testing functions and lines    #
+#######################################################################
 
-alldata <- PredRetR:::get_user_data()
-str(alldata)
+# old testing lines for getting input tables
+csv <- read.table("/data/Metabolite negativ H2O MAF.csv", sep=",") 
+tsv <- read.table("/data/Metabolite negativ H2O MAF.tsv", sep="\t")
+csvH <- read.table("/data/Metabolite negativ H2O MAF.csv", sep=",", header=T,  stringsAsFactors = F) 
+tsvH <- read.table("/data/Metabolite positiv H2O MAF.tsv", sep="\t", header=T, stringsAsFactors = F)
+data <- read.table("/data/m_MTBLS160_Exudate_metabolite_profiling_mass_spectrometry_targeted_v2_maf.tsv", sep="\t", header=T)
 
+# old: only use lines with PubChem database
+subdata<-tsvH[tsvH$database=='PubChem', ]
+
+#translating ChEBI to PubChem CID:
+##library(devtools)
+##install_github(repo="CTSgetR", username="dgrapov", ref="simple")
+#library(CTSgetR)
+#values <- CTSgetR(id=subdata$database_identifier,from="ChEBI",to="PubChem CID")
+#subdata$database_identifier<-values$value
+
+# old: create correct colnames
+colnames(subdata) <- sub('database_identifier','PubChem',colnames(subdata))
+colnames(subdata) <- sub('metabolite_identification','Compound',colnames(subdata))
+colnames(subdata) <- sub('retention_time','rt',colnames(subdata))
+
+# old: Test dataframe
 testcolnames <- c('ci_lower', 'ci_upper', 'generation', 'inchi', 'name', 'predicted_rt', 'pubchem', 'recorded_rt', 'suspect', 'sys_id', 'userID', 'username', 'time') 
 #, 'system')
 testcontent <- c(as.numeric(NA), as.numeric(NA), 0, as.character(NA), 'Testsubstanz', as.numeric(NA), 999999, 13, FALSE, sys_id, 1234, 'Test', time)
@@ -89,21 +112,11 @@ testcontent <- c(as.numeric(NA), as.numeric(NA), 0, as.character(NA), 'Testsubst
 testdf <- data.frame(testcontent)
 testdf <- t(testdf)
 colnames(testdf) <- testcolnames
-
 View(testdf)
 
-#upload data to predret function
-PredRet_upload_CSV <- function(data) {
-  # Convert data.frame to bson
-  bson_data = mongo.bson.from.df(data)
-  
-  
-  # add to table
-  mongo <- PredRet_connect() 
-  wrote = mongo.insert.batch(mongo, PredRet.env$namespaces$ns_rtdata, bson_data)
-  del <- mongo.disconnect(mongo)
-  del <- mongo.destroy(mongo)
-}
+#uploaddata <- massage(subdata)
+#sortiere spalten so wie input????
+#uploaddata <- uploaddata[c(12,13,10,5,2,11,4,3,14,1,8,9,6,7)]
 
 halle <- "IPB_Halle"
 #check systems, for error searching purposes
