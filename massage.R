85d4adec8cf87a2ce601f5e5e66892344aae147e
pneefe13 <patrik.neefe@gmx.de>
2016-09-07 12:07:36 +0200

    Added Inchi-Translation; Cleaned
    - as the pubchem2inchi from chemhelper didn't seem to work, an
    alternative was implemented with CTSgetR so that the PubChem CIDs can be
    translated to their inchi counterparts
    - added a bit of documentation
    - cleaned up the file visually

--- massage.R
+++ massage.R
@@ -1,6 +1,7 @@
 massage <- function (input) {
-  data = input
   
+  # get data
+  data = input
   colnames = tolower(colnames(data))
   cols_to_get = c("compound","rt","method","pubchem","inchi")
   select=rep(NA,length(cols_to_get))
@@ -8,6 +9,7 @@ massage <- function (input) {
     select[i] = grep(cols_to_get[i],colnames,fixed = T)[1]
   }
   
+  # Make sure data is unique
   data   =    data[,select[!is.na(select)]]
   data = unique(data)
   
@@ -23,8 +25,6 @@ massage <- function (input) {
     data = cbind.data.frame(data,inchi=NA)
   }
   
-  
-  
   # Make sure pubchem id is treated as integer
   if(any(colnames(data)=="pubchem")){
     data[,"pubchem"] = as.integer(data[,"pubchem"])
@@ -33,23 +33,29 @@ massage <- function (input) {
   # Make sure rt is treated as numeric
   data[,"recorded_rt"] = as.numeric(data[,"recorded_rt"])
   
+  # Test whether there are data without rt and exit if that is the case
   no_rt = is.na(data[,"recorded_rt"]) | is.nan(data[,"recorded_rt"])
   if(any(no_rt)) {paste("Error: Not all entries contain a retention time."); return}
   
   data =    data[!(no_rt),,drop=F]
   
+  # Get InChI from the PubChem CIDs
   if(any(colnames(data)=="inchi")){
+    # Specify no_inchi
     no_inchi = !grepl("InChI",data[,"inchi"],fixed=T)   &    !(is.na(data[,"pubchem"]) | is.nan(data[,"pubchem"]))
-    #next line does not work for some reason
-    #temp_data[no_inchi,"inchi"] = pubchem2inchi(    temp_data[no_inchi,"pubchem"]       )
-    data[no_inchi,"inchi"] = 0
-      #as.character(NA)
+    # Use CTSgetR to get the InChIs
+    data[no_inchi,"inchi"] = as.character(CTSgetR(id=data[no_inchi,"pubchem"], from="PubChem CID", to="InChI Code")[,2])
+    
+    # ATTENTION: If, for some reason, the above line with CTSgetR does not work,
+    # instead use the line below to still be able to upload the data (only with
+    # missing InChi)
+    #data[no_inchi,"inchi"] = 0
   }
   
   time = Sys.time()
   
+  # Set system to IPB_Halle
   halle <- "IPB_Halle"
-  #sys_name = halle
   sys_name = as.character(unlist(lapply(systems_in_db(),function(x) x$system_name)))  
   sys_id = unlist(lapply(systems_in_db(),function(x) as.character.mongo.oid(x$`_id`))  )
   idx = match(halle,sys_name)
@@ -59,21 +65,13 @@ massage <- function (input) {
     data = subset(data,select = -system_name) # Remove names and rely only on system ids  
   }
   
-  #system=halle zwischen time und userID
+  # Connect all to one dataframe
   data =data.frame(sys_id,data,time=time, userID=as.integer(1234),username=as.character("Test"),generation=as.integer(0),stringsAsFactors= FALSE)
   
- # if(!is.na(data)){
-  #TRUE LINE data <- cbind.data.frame(data,predicted_rt=as.numeric(NA),ci_lower=as.numeric(NA),ci_upper=as.numeric(NA),suspect=FALSE)
+  # Add columns of predicted rt, cis and suspect
   data <- cbind.data.frame(data,predicted_rt=0,ci_lower=0,ci_upper=0,suspect=FALSE)
-  # }
-  #if(!is.na(data)){
-   # data <- cbind.data.frame(data,predicted_rt=NA,ci_lower=NA,ci_upper=NA,suspect=FALSE)
-  #}
   
-   #correct name (not as vector???) 
-  #corrnames <- as.character(data$name)
-  #data$name <- corrnames
-  
+  # Return the dataframe
   out = data
   return(out)
   
